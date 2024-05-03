744c6331a41d49708b1b9d23966725e6b01b9e38
PopKorn0505 <chawakorn555@gmail.com>
2024-05-03 23:32:27 +0700

    finish dyncombo.html+js

--- week2/lecture03_js_practice_student/dynCombo.js
+++ week2/lecture03_js_practice_student/dynCombo.js
@@ -7,3 +7,6 @@ var helpers = ["Bob 1234 Bangkok", "Mary 5678 Phuket","Susan 9012 Nakorn Prathom
 // The event handler function to change the value of the 
 //  textarea
 //Add your code here
+function messages(x){
+    document.getElementById("adviceBox").value = helpers[x];
+}
